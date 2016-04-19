d60b9f6d02a6bee1d4c6a4cb106adda9e25a409d
jumpeihasegawa <jumpei.peipei@gmail.com>
2016-04-19 18:16:11 +0900

    Javaオブジェクト指向２の課題

---
+++ object2/src/java/org/mypackage/sample/BlackjackServlet.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Jumpei
+ */
+public class BlackjackServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        Dealer D = new Dealer();
+        ArrayList<Integer> Udeal = D.deal();
+
+        User U = new User();
+        U.setCard(Udeal, out);
+        boolean UcheckSum = U.checkSum();
+        while (UcheckSum == true) {
+            ArrayList<Integer> Uhit = D.hit(UcheckSum);
+            U.setCard(Uhit, out);
+            UcheckSum = U.checkSum();
+        }
+        int u = U.open(out);
+
+        out.print("<br><br>");
+        ArrayList<Integer> Ddeal = D.deal();
+        D.setCard(Ddeal, out);
+        boolean BcheckSum = D.checkSum();
+        while (BcheckSum == true) {
+            ArrayList<Integer> Dhit = D.hit(BcheckSum);
+            D.setCard(Dhit, out);
+            BcheckSum = D.checkSum();
+        }
+        int d = D.open(out);
+        
+        out.print("<br>");
+
+        if (u > 21 && d > 21 || u == d) {
+            out.print("引き分けです");
+        } else if (u > 21 || d > 21) {
+            if (u > d) {
+                out.print("ディーラーの勝ちです");
+            } else {
+                out.print("ユーザーの勝ちです");
+            }
+        } else if (u > d) {
+            out.print("ユーザーの勝ちです");
+        }else{
+            out.print("ディーラーの勝ちです");
+        }
+
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>オブジェクト指向２の課題</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ object2/src/java/org/mypackage/sample/Human.java
@@ -0,0 +1,182 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * @author Jumpei
+ */
+/**
+ * １．Humanという抽象クラスを作成し、以下を実装してください。 ・openというabstractな公開メソッドを用意してください。
+ * ・setCardというArrayListを引数とした、abstractな公開メソッドを用意してください。
+ * ・checkSumというabstractな公開メソッドを用意してください。 ・myCardsというArrayListの変数を用意してください。
+ */
+abstract class Human {
+
+    abstract public Integer open(PrintWriter out);
+
+    abstract public void setCard(ArrayList<Integer> mycard, PrintWriter out);
+
+    abstract public boolean checkSum();
+
+    ArrayList<Integer> myCards = new ArrayList<Integer>();
+
+}
+
+/**
+ * ２．１で作成した抽象クラスを継承して、以下のクラスを作成してください。 ・Dealerクラス ・Userクラス
+ */
+/**
+ * ３．まずはDealerクラスを完成させます。
+ * ・cardsというArrayListの変数を用意し、初期処理でこのcardsに全てのトランプを持たせてください。
+ * ・dealという公開メソッドを用意し、cardsからランダムで2枚のカードをArrayListにして返却してください。
+ * ・hitという公開メソッドを用意し、cardsからランダムで1枚のカードをArrayListにして返却してください。 / /**
+ * ４．DealerとUser両方に必要な処理を実装し、完成させます。
+ * ・setCardは、ArrayListで受けたカード情報をmyCardsに追加するように実装してください。
+ * ・checkSumは、myCardsを確認し、まだカードが必要ならtrue、必要無ければfalseを返却するように実装してください。
+ * ・openは、myCardsのカードの合計値を返却するように実装してください。
+ */
+class Dealer extends Human {
+
+    ArrayList<Integer> Cards = new ArrayList<Integer>();
+    ArrayList<Integer> Ldeal = new ArrayList<Integer>();
+    ArrayList<Integer> Lhit = new ArrayList<Integer>();
+    int goukei;
+    int T = 1;
+
+    Dealer() {
+        for (int j = 1; j <= 4; j++) {
+            for (int i = 1; i <= 13; i++) {
+                Cards.add(i);
+            }
+        }
+    }
+
+    public ArrayList<Integer> deal() {
+        Ldeal.clear();
+        Random r = new Random();
+        for (int i = 1; i <= 2; i++) {
+            Integer R = r.nextInt(Cards.size());
+            Ldeal.add(Cards.get(R));
+        }
+
+        return Ldeal;
+    }
+
+    public ArrayList<Integer> hit(boolean BcheckSum) {
+        Random r = new Random();
+        Lhit.clear();
+        if (BcheckSum == true) {
+            Integer R = r.nextInt(Cards.size());
+            Lhit.add(Cards.get(R));
+        }
+        return Lhit;
+    }
+
+    public Integer open(PrintWriter out) {
+        out.print("ディーラーのポイント"+goukei+"<br>");
+        myCards.clear();
+        return goukei;
+    }
+    
+    public void setCard(ArrayList<Integer> DsetCard, PrintWriter out) {
+        for (int i = 0; i < DsetCard.size(); i++) {
+            myCards.add(DsetCard.get(i));
+        }
+        
+        out.print("ディーラーのカード"+"("+ T++ +"ターン目)"+"<br>");
+        for (int i = 0; i < myCards.size(); i++) {
+            out.print("カードの"+myCards.get(i)+"<br>");
+        }
+    }
+
+    public boolean checkSum() {
+        for (int i = 0; i < myCards.size(); i++) {
+            if (myCards.get(i) >= 11) {
+                myCards.set(i, 10);
+            }
+        }
+
+        goukei = 0;
+        for (int i = 0; i < myCards.size(); i++) {
+            goukei += myCards.get(i);
+        }
+        if (goukei <= 11 && goukei >= 6) {
+            for (int i = 0; i < myCards.size(); i++) {
+                if (myCards.get(i) == 1) {
+                    myCards.set(i, 11);
+                    break;
+                }
+            }
+        }
+        goukei = 0;
+        for (int i = 0; i < myCards.size(); i++) {
+            goukei += myCards.get(i);
+        }
+        if (goukei < 16) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+class User extends Human {
+
+    int goukei;
+    int T = 1;
+
+    public Integer open(PrintWriter out) {
+        out.print("ユーザーのポイント" + goukei);
+        return goukei;
+    }
+
+    public void setCard(ArrayList<Integer> UsetCard, PrintWriter out) {
+        for (int i = 0; i < UsetCard.size(); i++) {
+            myCards.add(UsetCard.get(i));
+        }
+        out.print("ユーザーのカード"+"("+ T++ +"ターン目)"+"<br>");
+        for (int i = 0; i < myCards.size(); i++) {
+            out.print("カードの"+myCards.get(i)+"<br>");
+        }
+    }
+
+    public boolean checkSum() {
+        for (int i = 0; i < myCards.size(); i++) {
+            if (myCards.get(i) >= 11) {
+                myCards.set(i, 10);
+            }
+        }
+
+        goukei = 0;
+        for (int i = 0; i < myCards.size(); i++) {
+            goukei += myCards.get(i);
+        }
+        if (goukei <= 11 && goukei >= 6) {
+            for (int i = 0; i < myCards.size(); i++) {
+                if (myCards.get(i) == 1) {
+                    myCards.set(i, 11);
+                    break;
+                }
+            }
+        }
+
+        goukei = 0;
+        for (int i = 0; i < myCards.size(); i++) {
+            goukei += myCards.get(i);
+        }
+
+        if (goukei < 16) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
